a720e0d78f959217c5bff5fdeefdb7a3552d67ca
Robert Ludvigsen <robertludvigsen@Roberts-MacBook-Pro.local>
2015-10-19 03:02:42 -0400

    late late night commit, getting values, still no clue

--- app.js
+++ app.js
@@ -59,121 +59,128 @@
 // //   if (playerScore < 21)
 
 // // }
+window.onload = function () {
 
 
 
 
 
+    var startingDeck = [
 
-var startingDeck = [
+            { name: 'Ace', value : 1, suit: 'clubs' },
+            { name: '2', value : 2, suit: 'clubs' },
+            { name: '3', value: 3, suit: 'clubs' },
+            { name: '4', value: 4 , suit: 'clubs' },
+            { name: '5', value: 5, suit: 'clubs' },
+            { name: '6', value: 6, suit: 'clubs' },
+            { name: '7', value: 7, suit: 'clubs' },
+            { name: '8', value: 8, suit: 'clubs' },
+            { name: '9', value: 9, suit: 'clubs' },
+            { name: '10', value: 10, suit: 'clubs' },
+            { name: 'Jack', value: 10, suit: 'clubs' },
+            { name: 'Queen', value: 10, suit: 'clubs' },
+            { name: 'King', value: 10, suit: 'clubs' },
+            { name: 'Ace', value : 1, suit: 'diamonds' },
+            { name: '2', value : 2, suit: 'diamonds' },
+            { name: '3', value: 3, suit: 'diamonds' },
+            { name: '4', value: 4 , suit: 'diamonds' },
+            { name: '5', value: 5, suit: 'diamonds' },
+            { name: '6', value: 6, suit: 'diamonds' },
+            { name: '7', value: 7, suit: 'diamonds' },
+            { name: '8', value: 8, suit: 'diamonds' },
+            { name: '9', value: 9, suit: 'diamonds' },
+            { name: '10', value: 10, suit: 'diamonds' },
+            { name: 'Jack', value: 10, suit: 'diamonds' },
+            { name: 'Queen', value: 10, suit: 'diamonds' },
+            { name: 'King', value: 10, suit: 'diamonds' },
+            { name: 'Ace', value : 1, suit: 'hearts' },
+            { name: '2', value : 2, suit: 'hearts' },
+            { name: '3', value: 3, suit: 'hearts' },
+            { name: '4', value: 4 , suit: 'hearts' },
+            { name: '5', value: 5, suit: 'hearts' },
+            { name: '6', value: 6, suit: 'hearts' },
+            { name: '7', value: 7, suit: 'hearts' },
+            { name: '8', value: 8, suit: 'hearts' },
+            { name: '9', value: 9, suit: 'hearts' },
+            { name: '10', value: 10, suit: 'hearts' },
+            { name: 'Jack', value: 10, suit: 'hearts' },
+            { name: 'Queen', value: 10, suit: 'hearts' },
+            { name: 'King', value: 10, suit: 'hearts' },
+            { name: 'Ace', value : 1, suit: 'diamonds' },
+            { name: '2', value : 2, suit: 'diamonds' },
+            { name: '3', value: 3, suit: 'diamonds' },
+            { name: '4', value: 4 , suit: 'diamonds' },
+            { name: '5', value: 5, suit: 'diamonds' },
+            { name: '6', value: 6, suit: 'diamonds' },
+            { name: '7', value: 7, suit: 'diamonds' },
+            { name: '8', value: 8, suit: 'diamonds' },
+            { name: '9', value: 9, suit: 'diamonds' },
+            { name: '10', value: 10, suit: 'diamonds' },
+            { name: 'Jack', value: 10, suit: 'diamonds' },
+            { name: 'Queen', value: 10, suit: 'diamonds' },
+            { name: 'King', value: 10, suit: 'diamonds' }
+    ];  
 
-        { name: 'Ace', value : 1, suit: 'clubs' },
-        { name: '2', value : 2, suit: 'clubs' },
-        { name: '3', value: 3, suit: 'clubs' },
-        { name: '4', value: 4 , suit: 'clubs' },
-        { name: '5', value: 5, suit: 'clubs' },
-        { name: '6', value: 6, suit: 'clubs' },
-        { name: '7', value: 7, suit: 'clubs' },
-        { name: '8', value: 8, suit: 'clubs' },
-        { name: '9', value: 9, suit: 'clubs' },
-        { name: '10', value: 10, suit: 'clubs' },
-        { name: 'Jack', value: 10, suit: 'clubs' },
-        { name: 'Queen', value: 10, suit: 'clubs' },
-        { name: 'King', value: 10, suit: 'clubs' },
-        { name: 'Ace', value : 1, suit: 'diamonds' },
-        { name: '2', value : 2, suit: 'diamonds' },
-        { name: '3', value: 3, suit: 'diamonds' },
-        { name: '4', value: 4 , suit: 'diamonds' },
-        { name: '5', value: 5, suit: 'diamonds' },
-        { name: '6', value: 6, suit: 'diamonds' },
-        { name: '7', value: 7, suit: 'diamonds' },
-        { name: '8', value: 8, suit: 'diamonds' },
-        { name: '9', value: 9, suit: 'diamonds' },
-        { name: '10', value: 10, suit: 'diamonds' },
-        { name: 'Jack', value: 10, suit: 'diamonds' },
-        { name: 'Queen', value: 10, suit: 'diamonds' },
-        { name: 'King', value: 10, suit: 'diamonds' },
-        { name: 'Ace', value : 1, suit: 'hearts' },
-        { name: '2', value : 2, suit: 'hearts' },
-        { name: '3', value: 3, suit: 'hearts' },
-        { name: '4', value: 4 , suit: 'hearts' },
-        { name: '5', value: 5, suit: 'hearts' },
-        { name: '6', value: 6, suit: 'hearts' },
-        { name: '7', value: 7, suit: 'hearts' },
-        { name: '8', value: 8, suit: 'hearts' },
-        { name: '9', value: 9, suit: 'hearts' },
-        { name: '10', value: 10, suit: 'hearts' },
-        { name: 'Jack', value: 10, suit: 'hearts' },
-        { name: 'Queen', value: 10, suit: 'hearts' },
-        { name: 'King', value: 10, suit: 'hearts' },
-        { name: 'Ace', value : 1, suit: 'diamonds' },
-        { name: '2', value : 2, suit: 'diamonds' },
-        { name: '3', value: 3, suit: 'diamonds' },
-        { name: '4', value: 4 , suit: 'diamonds' },
-        { name: '5', value: 5, suit: 'diamonds' },
-        { name: '6', value: 6, suit: 'diamonds' },
-        { name: '7', value: 7, suit: 'diamonds' },
-        { name: '8', value: 8, suit: 'diamonds' },
-        { name: '9', value: 9, suit: 'diamonds' },
-        { name: '10', value: 10, suit: 'diamonds' },
-        { name: 'Jack', value: 10, suit: 'diamonds' },
-        { name: 'Queen', value: 10, suit: 'diamonds' },
-        { name: 'King', value: 10, suit: 'diamonds' }
-];  
-
-var playerHand = new Array();
-var dealerHand = new Array();
-
-
-function shuffle(array) {
-  var currentIndex = array.length, temporaryValue, randomIndex ;
-
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
-}
-shuffle(startingDeck);
-console.log(startingDeck);
-
-playerHand.push(startingDeck[0]);
-playerHand.push(startingDeck[1]);
-dealerHand.push(startingDeck[2]);
-dealerHand.push(startingDeck[3]);
-console.log(playerHand);
-console.log(dealerHand);
-
-var playerHandValue = (playerHand[0].value + playerHand[1].value);
-var dealerHandValue = (dealerHand[0].value + dealerHand[1].value);
-
-console.log(playerHandValue);
-console.log(dealerHandValue);
-
-var playerHandDisplay = document.createElement("div");
-var playerHandDisplay.innerHTML = "You were dealt the " + playerHand[0].name + " of " + playerHand[0].suit + " and the " + playerHand[1].name + " of " playerHand[1].suit + " for a value of " + playerHandValue;
-playerHandDisplay.body.appendChild(handInfo);
-
-console.log(playerHandDisplay);
+    var playerHand = new Array();
+    var dealerHand = new Array();
 
 
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex ;
 
-var hitBut = document.getElementById("hit");
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
 
-hitBut.onClick = function () {
-    for (var i = 3; i < 52; i++) {
-        playerHand.push(startingDeck[i]);
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+
+      return array;
     }
+    shuffle(startingDeck);
+    console.log(startingDeck);
+
+    playerHand.push(startingDeck[0]);
+    playerHand.push(startingDeck[1]);
+    dealerHand.push(startingDeck[2]);
+    dealerHand.push(startingDeck[3]);
+    console.log(playerHand);
+    console.log(dealerHand);
+
+    var playerHandValue = (playerHand[0].value + playerHand[1].value);
+    var dealerHandValue = (dealerHand[0].value + dealerHand[1].value);
+
+    console.log(playerHandValue);
+    console.log(dealerHandValue);
+
+    console.log(playerHand[0].name + " of " + playerHand[0].suit);
+    console.log(playerHand[1].name + " of " + playerHand[1].suit);
+
+    console.log(dealerHand[0].name + " of " + dealerHand[0].suit);
+    console.log(dealerHand[1].name + " of " + dealerHand[1].suit);
+
+
+
+
+    var playerHandDisplay = document.createElement("div");
+    var playerHandDisplay.innerHTML = "You were dealt the " + playerHand[0].name + " of " + playerHand[0].suit + " and the " + playerHand[1].name + " of " playerHand[1].suit + " for a value of " + playerHandValue;
+    playerHandDisplay.body.appendChild(handInfo);
+
+    console.log(playerHandDisplay);
 }
+// var hitBut = document.getElementById("hit");
+
+// hitBut.onClick = function () {
+//     for (var i = 3; i < 52; i++) {
+//         playerHand.push(startingDeck[i]);
+//     }
+// }
 
 
 /////I need to get two cards for dealer and player out of this^
@@ -231,119 +238,7 @@ hitBut.onClick = function () {
 // • A function should do 1 thing well
 // • Break down Pseudo code into 1 action
 
-////========================================================
-// var playerIndex = new Array();
 
-// var playerHand = new Array();
-// var playerHandSuit = new Array();
-// var dealerHand = new Array();
-// var dealerHandSuit = new Array ();
-
-// var playerHandValueHard = 0;
-// var playerHandValueSoft = 0;
-
-// var dealerHandValue = 0;
-// var dealerHandValueSoft = 0;
-
-// var playerNumber = 0;
-// var numberOfPlayers = 1;
-
-// var playerCardNumber = 0;
-
-// function stick () {
-
-//     playerIndex[playerNumber] = {playerHand, playerHandSuit, playerHandValueHard, playerHandValueSoft};
-//     playerNumber = playerNumber + 1;
-//     if (playerNumber >= numberOfPlayers) {
-
-//     }
-
-// }
-
-// function dealerHit () {
-
-//     dealerHand[playerCardNumber] = card[0];
-//     dealerHandSuit[playerCardNumber] = suit;
-//     dealerHandValueHard = playerHandValueHard + card[1];
-//     dealerHandValueSoft = playerHandValueSoft + card[2];
-
-// }
-
-
-// function playerHit () {
-
-//     playerCardNumber = playerCardNumber + 1;
-//     var suit = randSuit();
-//     var card = randCard();
-    
-//     playerHand[playerCardNumber] = card[0];
-//     playerHandSuit[playerCardNumber] = suit;
-//     playerHandValueHard = playerHandValueHard + card[1];
-//     playerHandValueSoft = playerHandValueSoft + card[2];
-
-// }
-
-// function randCard() {
-//     var cardArray = new Array ();
-//     var randCard = random (1, 13);
-//     switch(randCard) {
-//         case 1:
-//             cardArray[0] = "A";
-//             cardArray[1] = 1;
-//             cardArray[2] = 11;
-//             break;
-//         case 11:
-//             cardArray[0] = "J";
-//             cardArray[1] = 10;
-//             cardArray[2] = 10;
-//             break;
-//         case 12:
-//             cardArray[0] = "Q";
-//             cardArray[1] = 10;
-//             cardArray[2] = 10;
-//             break;
-//         case 13:
-//             cardArray[0] = "K";
-//             cardArray[1] = 10;
-//             cardArray[2] = 10;
-//             break;
-//         default:
-//             cardArray[0] = randCard;
-//             cardArray[1] = randCard;
-//             cardArray[2] = randCard;
-//     }
-//     return cardArray;
-
-// }
-
-// function randSuit () {
-//     var suit;
-//     rand = random(0, 3);
-//     switch(rand) {
-//         case 0:
-//             suit = "hearts"
-//             break;
-//         case 1: 
-//             suit = "Spades"
-//             break;
-//         case 2:
-//             suit = "Diamonds"
-//             break;
-//         case 3:
-//             suit = "Clubs"
-//             break;
-//         default: 
-//             alert("error getting suit");
-//     }
-
-//     return suit;
-// }
-
-// function random (x,y) {
-//         ///
-//     return Math.floor(Math.random()*(y-x+1))+ x;
-// }
-////==============================================================
 
 
 
